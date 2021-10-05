985e31f846728d8fb1e502a6dbbe3edc44045f68
NonSpicyBurrito <burritoyangster@gmail.com>
2021-10-06 07:40:45 +0800

    Ch.02

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,13 @@
-import { Multiply, visualize } from 'sonolus.js'
+import { Code, compile, Multiply, visualize } from 'sonolus.js'
 
-console.log(visualize(Multiply(Math.PI, 5, 5)))
+console.log(Multiply(Math.PI, 5, 5))
+
+const environment = { nodes: [] }
+compile(Multiply(Math.PI, 5, 5), environment)
+console.log(environment.nodes)
+
+function calculateCircleArea(radius: Code<number>) {
+    return Multiply(Math.PI, radius, radius)
+}
+
+console.log(visualize(calculateCircleArea(5)))
