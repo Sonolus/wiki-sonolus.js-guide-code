8c213973a57fdb2ad29588ecda9aa27e0774507d
NonSpicyBurrito <burritoyangster@gmail.com>
2021-10-07 05:31:03 +0800

    Ch.15

--- src/engine/data/scripts/note.ts
+++ src/engine/data/scripts/note.ts
@@ -1,15 +1,23 @@
 import { SkinSprite } from 'sonolus-core'
 import {
     Add,
+    And,
     createEntityData,
     Draw,
     EntityMemory,
+    Greater,
     GreaterOr,
+    InputOffset,
+    Not,
+    Or,
     Pointer,
     Remap,
     Script,
     Subtract,
+    TemporaryMemory,
     Time,
+    TouchST,
+    TouchStarted,
 } from 'sonolus.js'
 
 class EntityDataPointer extends Pointer {
@@ -23,17 +31,31 @@ const EntityData = createEntityData(EntityDataPointer)
 export function note(): Script {
     const spawnTime = EntityMemory.to<number>(0)
     const z = EntityMemory.to<number>(1)
+    const minInputTime = EntityMemory.to<number>(2)
+    const maxInputTime = EntityMemory.to<number>(3)
 
     const yCurrent = EntityMemory.to<number>(32)
+    const inputState = EntityMemory.to<boolean>(33)
 
     const preprocess = [
         spawnTime.set(Subtract(EntityData.time, 1)),
         z.set(Subtract(1000, EntityData.time)),
+        minInputTime.set(Add(EntityData.time, -0.2, InputOffset)),
+        maxInputTime.set(Add(EntityData.time, 0.2, InputOffset)),
     ]
 
     const spawnOrder = Add(spawnTime, 1000)
 
     const shouldSpawn = GreaterOr(Time, spawnTime)
+
+    const isTouchOccupied = TemporaryMemory.to<boolean>(0)
+
+    const touch = And(
+        TouchStarted,
+        GreaterOr(TouchST, minInputTime),
+        Not(isTouchOccupied),
+        [inputState.set(true), isTouchOccupied.set(true)]
+    )
 
     const radius = 0.2
     const yFrom = 1 + radius
@@ -44,7 +66,7 @@ export function note(): Script {
     const top = Add(yCurrent, radius)
     const bottom = Subtract(yCurrent, radius)
 
-    const updateParallel = [
+    const updateParallel = Or(inputState, Greater(Time, maxInputTime), [
         yCurrent.set(Remap(spawnTime, EntityData.time, yFrom, yTo, Time)),
         Draw(
             SkinSprite.NoteHeadCyan,
@@ -59,12 +81,13 @@ export function note(): Script {
             z,
             1
         ),
-    ]
+    ])
 
     return {
         preprocess,
         spawnOrder,
         shouldSpawn,
+        touch,
         updateParallel,
     }
 }
