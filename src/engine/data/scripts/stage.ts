99c2a8d312db8fcf1a6ceaa4e3203cac0de02da1
NonSpicyBurrito <burritoyangster@gmail.com>
2021-10-07 04:45:25 +0800

    Ch.12

--- src/engine/data/scripts/stage.ts
+++ src/engine/data/scripts/stage.ts
@@ -2,17 +2,25 @@ import { SkinSprite } from 'sonolus-core'
 import {
     Draw,
     EntityInfo,
+    EntityMemory,
     Equal,
+    If,
     Multiply,
+    Or,
     ScreenAspectRatio,
     Script,
     State,
+    TouchEnded,
 } from 'sonolus.js'
 
 export function stage(): Script {
+    const anyTouch = EntityMemory.to<boolean>(0)
+
     const spawnOrder = 1
 
     const shouldSpawn = Equal(EntityInfo.of(0).state, State.Despawned)
+
+    const touch = Or(TouchEnded, anyTouch.set(true))
 
     const yCenter = -0.6
     const thickness = 0.1
@@ -23,23 +31,27 @@ export function stage(): Script {
     const top = yCenter + thickness / 2
     const bottom = yCenter - thickness / 2
 
-    const updateParallel = Draw(
-        SkinSprite.JudgmentLine,
-        left,
-        bottom,
-        left,
-        top,
-        right,
-        top,
-        right,
-        bottom,
-        0,
-        1
-    )
+    const updateParallel = [
+        Draw(
+            SkinSprite.JudgmentLine,
+            left,
+            bottom,
+            left,
+            top,
+            right,
+            top,
+            right,
+            bottom,
+            0,
+            If(anyTouch, 1, 0.5)
+        ),
+        anyTouch.set(false),
+    ]
 
     return {
         spawnOrder,
         shouldSpawn,
+        touch,
         updateParallel,
     }
 }
