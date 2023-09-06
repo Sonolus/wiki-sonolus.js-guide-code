444019f8f342c2d1716a96863d9616f34a2eef1c
NonSpicyBurrito <burritoyangster@gmail.com>
2023-09-06 22:10:11 +0800

    Initial commit

--- shared/src/engine/configuration/options.ts
+++ shared/src/engine/configuration/options.ts
@@ -1,3 +1,23 @@
-import { EngineConfigurationOption } from 'sonolus-core'
+import { EngineConfigurationOption, NameText, UnitText } from 'sonolus-core'
 
-export const optionsDefinition = {} satisfies Record<string, EngineConfigurationOption>
+export const optionsDefinition = {
+    speed: {
+        name: NameText.LevelSpeed,
+        standard: true,
+        type: 'slider',
+        def: 1,
+        min: 0.5,
+        max: 2,
+        step: 0.05,
+        unit: UnitText.Percentage,
+    },
+    noteSize: {
+        name: NameText.NoteSize,
+        type: 'slider',
+        def: 1,
+        min: 0.1,
+        max: 2,
+        step: 0.05,
+        unit: UnitText.Percentage,
+    },
+} satisfies Record<string, EngineConfigurationOption>
