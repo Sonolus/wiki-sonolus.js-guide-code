89968c6eef2a1025e858efbcf7af0de926dc0628
NonSpicyBurrito <burritoyangster@gmail.com>
2023-09-07 21:57:15 +0800

    Initial commit

--- preview/src/engine/previewData/panel.ts
+++ preview/src/engine/previewData/panel.ts
@@ -7,4 +7,16 @@ export const panel = {
     get count() {
         return Math.ceil(chart.duration / this.h)
     },
+
+    getX(time: number) {
+        return Math.floor(time / this.h) * this.w
+    },
+
+    getY(time: number) {
+        return time % this.h
+    },
+
+    getPos(time: number) {
+        return new Vec(this.getX(time), this.getY(time))
+    },
 }
