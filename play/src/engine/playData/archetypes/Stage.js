6793781dc6faba06fe82ed15b62d0d0f0202944f
NonSpicyBurrito <burritoyangster@gmail.com>
2023-09-08 02:18:41 +0800

    Initial commit

--- play/src/engine/playData/archetypes/Stage.js
+++ play/src/engine/playData/archetypes/Stage.js
@@ -1,3 +1,7 @@
+import { judgeLine } from '../judgeLine.js'
+import { note } from '../note.js'
+import { skin } from '../skin.js'
+
 export class Stage extends Archetype {
     spawnOrder() {
         return 1
@@ -8,6 +12,13 @@ export class Stage extends Archetype {
     }
 
     updateParallel() {
-        debug.log(time.now)
+        const layout = new Rect({
+            l: judgeLine.l,
+            r: judgeLine.r,
+            t: 1 - note.radius / 4,
+            b: 1 + note.radius / 4,
+        })
+
+        skin.sprites.judgeLine.draw(layout, 0, touches.count ? 1 : 0.5)
     }
 }
