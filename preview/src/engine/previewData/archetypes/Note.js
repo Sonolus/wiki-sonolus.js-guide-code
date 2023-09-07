08fd5043aa8d8c4e2dd41e4889fa33cebeafbd58
NonSpicyBurrito <burritoyangster@gmail.com>
2023-09-08 02:49:48 +0800

    Initial commit

--- preview/src/engine/previewData/archetypes/Note.js
+++ preview/src/engine/previewData/archetypes/Note.js
@@ -1,5 +1,9 @@
 import { EngineArchetypeDataName } from 'sonolus-core'
+import { options } from '../../configuration/options.js'
 import { chart } from '../chart.js'
+import { panel } from '../panel.js'
+import { scaledScreen } from '../scaledScreen.js'
+import { skin } from '../skin.js'
 
 export class Note extends Archetype {
     data = this.defineData({
@@ -8,5 +12,15 @@ export class Note extends Archetype {
 
     preprocess() {
         chart.duration = Math.max(chart.duration, bpmChanges.at(this.data.beat).time)
+    }
+
+    render() {
+        const time = bpmChanges.at(this.data.beat).time
+        const pos = panel.getPos(time)
+
+        const layout = Rect.one.mul(options.noteSize).scale(1, scaledScreen.wToH).add(pos)
+        const z = 1000 - time
+
+        skin.sprites.note.draw(layout, z, 1)
     }
 }
--- preview/src/engine/previewData/panel.js
+++ preview/src/engine/previewData/panel.js
@@ -7,4 +7,16 @@ export const panel = {
     get count() {
         return Math.ceil(chart.duration / this.h)
     },
+
+    getX(time) {
+        return Math.floor(time / this.h) * this.w
+    },
+
+    getY(time) {
+        return time % this.h
+    },
+
+    getPos(time) {
+        return new Vec(this.getX(time), this.getY(time))
+    },
 }
