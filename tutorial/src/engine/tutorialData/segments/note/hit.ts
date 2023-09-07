a8f3fbff141befec0d1945c76511a89b72c0ad61
NonSpicyBurrito <burritoyangster@gmail.com>
2023-09-08 00:46:23 +0800

    Initial commit

--- tutorial/src/engine/tutorialData/segments/note/hit.ts
+++ tutorial/src/engine/tutorialData/segments/note/hit.ts
@@ -1,7 +1,16 @@
 import { effect } from '../../effect.js'
+import { note } from '../../note.js'
+import { particle } from '../../particle.js'
 
 export const noteHit = {
     enter() {
         effect.clips.perfect.play(0)
+
+        const layout = Rect.one
+            .mul(2 * note.radius)
+            .scale(1, -1)
+            .translate(0, 1)
+
+        particle.effects.note.spawn(layout, 0.3, false)
     },
 }
