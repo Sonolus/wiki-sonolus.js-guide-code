5fa4391a208c24dc037c304eb88f2eadb37fdb5d
NonSpicyBurrito <burritoyangster@gmail.com>
2023-09-08 02:13:56 +0800

    Initial commit

--- play/src/engine/playData/archetypes/Initialization.js
+++ play/src/engine/playData/archetypes/Initialization.js
@@ -1,5 +1,20 @@
+import { particle } from '../particle.js'
+import { skin } from '../skin.js'
+
 export class Initialization extends Archetype {
     preprocess() {
+        const noteRadius = 0.2
+        const judgeLineY = -0.6
+
+        const t = screen.t + noteRadius
+        const b = judgeLineY
+        const h = t - b
+
+        const transform = Mat.identity.scale(h, -h).translate(0, t)
+
+        skin.transform.set(transform)
+        particle.transform.set(transform)
+
         ui.menu.set({
             anchor: screen.rect.lt.add(new Vec(0.05, -0.05)),
             pivot: { x: 0, y: 1 },
