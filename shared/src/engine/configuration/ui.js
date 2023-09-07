5188d4afc07e71bc118d53284a2f501137f7e0ec
NonSpicyBurrito <burritoyangster@gmail.com>
2023-09-08 02:26:29 +0800

    Initial commit

--- shared/src/engine/configuration/ui.js
+++ shared/src/engine/configuration/ui.js
@@ -43,17 +43,17 @@ export const ui = {
         },
         alpha: {
             from: 1,
-            to: 1,
-            duration: 0,
-            ease: 'None',
+            to: 0,
+            duration: 0.2,
+            ease: 'OutCubic',
         },
     },
     comboAnimation: {
         scale: {
-            from: 1,
+            from: 1.2,
             to: 1,
-            duration: 0,
-            ease: 'None',
+            duration: 0.2,
+            ease: 'InCubic',
         },
         alpha: {
             from: 1,
