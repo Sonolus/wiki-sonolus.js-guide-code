27e7b1625ff4f14738c76f1af62709919c2cd9c2
NonSpicyBurrito <burritoyangster@gmail.com>
2023-09-08 00:34:01 +0800

    Initial commit

--- tutorial/src/engine/tutorialData/components/initialization.ts
+++ tutorial/src/engine/tutorialData/components/initialization.ts
@@ -1,5 +1,27 @@
+import { judgeLine } from '../judgeLine.js'
+import { note } from '../note.js'
+import { particle } from '../particle.js'
+import { skin } from '../skin.js'
+
 export const initialization = {
     preprocess() {
+        const noteRadius = 0.2
+        const judgeLineY = -0.6
+
+        const t = screen.t + noteRadius
+        const b = judgeLineY
+        const h = t - b
+
+        const transform = Mat.identity.scale(h, -h).translate(0, t)
+
+        skin.transform.set(transform)
+        particle.transform.set(transform)
+
+        judgeLine.l = screen.l / h
+        judgeLine.r = screen.r / h
+
+        note.radius = noteRadius / h
+
         ui.menu.set({
             anchor: screen.rect.lt.add(new Vec(0.05, -0.05)),
             pivot: { x: 0, y: 1 },
