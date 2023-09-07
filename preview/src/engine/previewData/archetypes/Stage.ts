ce68bf5ccd8c1feeb05d181f5faac0069d5019ff
NonSpicyBurrito <burritoyangster@gmail.com>
2023-09-07 19:54:13 +0800

    Initial commit

---
+++ preview/src/engine/previewData/archetypes/Stage.ts
@@ -0,0 +1,49 @@
+import { panel } from '../panel.js'
+import { skin } from '../skin.js'
+
+export class Stage extends Archetype {
+    render() {
+        this.renderPanels()
+    }
+
+    renderPanels() {
+        for (let i = 0; i < panel.count; i++) {
+            const x = i * panel.w
+
+            const b = 0
+            const t = panel.h
+
+            skin.sprites.stageMiddle.draw(
+                new Rect({
+                    l: -1.5,
+                    r: 1.5,
+                    b,
+                    t,
+                }).translate(x, 0),
+                0,
+                1,
+            )
+
+            skin.sprites.stageLeftBorder.draw(
+                new Rect({
+                    l: -1.75,
+                    r: -1.5,
+                    b,
+                    t,
+                }).translate(x, 0),
+                0,
+                1,
+            )
+            skin.sprites.stageRightBorder.draw(
+                new Rect({
+                    l: 1.5,
+                    r: 1.75,
+                    b,
+                    t,
+                }).translate(x, 0),
+                0,
+                1,
+            )
+        }
+    }
+}
