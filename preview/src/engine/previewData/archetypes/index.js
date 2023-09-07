73ea609a312c5d251a3a477fce7af1308405bc09
NonSpicyBurrito <burritoyangster@gmail.com>
2023-09-08 02:53:08 +0800

    Initial commit

--- preview/src/engine/previewData/archetypes/index.js
+++ preview/src/engine/previewData/archetypes/index.js
@@ -1,3 +1,5 @@
+import { EngineArchetypeName } from 'sonolus-core'
+import { BpmChange } from './BpmChange.js'
 import { Initialization } from './Initialization.js'
 import { Note } from './Note.js'
 import { Stage } from './Stage.js'
@@ -6,4 +8,5 @@ export const archetypes = defineArchetypes({
     Initialization,
     Stage,
     Note,
+    [EngineArchetypeName.BpmChange]: BpmChange,
 })
