a6fc4092f60d58a32e6dbb0d9633a5077f3f7b80
NonSpicyBurrito <burritoyangster@gmail.com>
2023-09-08 00:37:48 +0800

    Initial commit

--- tutorial/src/engine/tutorialData/tutorial.ts
+++ tutorial/src/engine/tutorialData/tutorial.ts
@@ -1,4 +1,5 @@
 import { initialization } from './components/initialization.js'
+import { noteDisplay } from './components/noteDisplay.js'
 import { stage } from './components/stage.js'
 import { segment } from './segment.js'
 import { exampleFall } from './segments/example/fall.js'
@@ -6,7 +7,7 @@ import { exampleFrozen } from './segments/example/frozen.js'
 import { exampleHit } from './segments/example/hit.js'
 import { exampleIntro } from './segments/example/intro.js'
 
-const components = [initialization, stage] as const
+const components = [initialization, stage, noteDisplay] as const
 
 const segments = [exampleIntro, exampleFall, exampleFrozen, exampleHit] as const
 
