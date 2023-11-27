6efb5d4f1890d4de790c02d23a007c1f033b3e26
Burrito <burritoyangster@gmail.com>
2023-11-28 05:56:06 +0800

    Initial commit

--- watch/src/engine/watchData/archetypes/Stage.js
+++ watch/src/engine/watchData/archetypes/Stage.js
@@ -1,3 +1,7 @@
+import { judgeLine } from '../judgeLine.js'
+import { note } from '../note.js'
+import { skin } from '../skin.js'
+
 export class Stage extends Archetype {
     spawnTime() {
         return -999999
@@ -8,6 +12,13 @@ export class Stage extends Archetype {
     }
 
     updateParallel() {
-        debug.log(time.now)
+        const layout = new Rect({
+            l: judgeLine.l,
+            r: judgeLine.r,
+            t: 1 - note.radius / 4,
+            b: 1 + note.radius / 4,
+        })
+
+        skin.sprites.judgeLine.draw(layout, 0, 0.5)
     }
 }
