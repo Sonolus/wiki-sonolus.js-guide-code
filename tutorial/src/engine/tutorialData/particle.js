73fd81552b00c4e277414c1e554119401f8294a9
Burrito <burritoyangster@gmail.com>
2024-12-05 01:29:45 +0800

    Initial commit

--- tutorial/src/engine/tutorialData/particle.js
+++ tutorial/src/engine/tutorialData/particle.js
@@ -1,3 +1,7 @@
+import { ParticleEffectName } from '@sonolus/core'
+
 export const particle = defineParticle({
-    effects: {},
+    effects: {
+        note: ParticleEffectName.NoteCircularTapCyan,
+    },
 })
