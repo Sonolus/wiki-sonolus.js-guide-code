47c3e3ac087752cdec12d230570584890e018aae
Burrito <burritoyangster@gmail.com>
2024-12-05 01:28:52 +0800

    Initial commit

--- watch/src/engine/watchData/archetypes/index.js
+++ watch/src/engine/watchData/archetypes/index.js
@@ -1,7 +1,9 @@
 import { Initialization } from './Initialization.js'
+import { Note } from './Note.js'
 import { Stage } from './Stage.js'
 
 export const archetypes = defineArchetypes({
     Initialization,
     Stage,
+    Note,
 })
