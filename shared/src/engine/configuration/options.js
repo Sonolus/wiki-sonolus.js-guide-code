abdc0257cecb160e0305faa5bbd9e350ab972197
Burrito <burritoyangster@gmail.com>
2024-12-05 01:28:42 +0800

    Initial commit

--- shared/src/engine/configuration/options.js
+++ shared/src/engine/configuration/options.js
@@ -1,2 +1,24 @@
+import { Text } from '@sonolus/core'
+
 /** @satisfies {Record<string, import('@sonolus/core').EngineConfigurationOption>} */
-export const optionsDefinition = {}
+export const optionsDefinition = {
+    speed: {
+        name: Text.Speed,
+        standard: true,
+        type: 'slider',
+        def: 1,
+        min: 0.5,
+        max: 2,
+        step: 0.05,
+        unit: Text.PercentageUnit,
+    },
+    noteSize: {
+        name: Text.NoteSize,
+        type: 'slider',
+        def: 1,
+        min: 0.1,
+        max: 2,
+        step: 0.05,
+        unit: Text.PercentageUnit,
+    },
+}
