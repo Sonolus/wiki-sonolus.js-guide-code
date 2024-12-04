1f74efe63fdbdb38fcb803f148a3e5aca57275b8
Burrito <burritoyangster@gmail.com>
2024-12-05 01:13:53 +0800

    Initial commit

--- play/src/engine/playData/archetypes/Note.ts
+++ play/src/engine/playData/archetypes/Note.ts
@@ -3,6 +3,7 @@ import { EngineArchetypeDataName } from '@sonolus/core'
 import { buckets } from '../buckets.js'
 import { effect } from '../effect.js'
 import { note } from '../note.js'
+import { particle } from '../particle.js'
 import { skin } from '../skin.js'
 import { bucketWindows, windows } from '../windows.js'
 import { isUsed, markAsUsed } from './InputManager.js'
@@ -82,6 +83,13 @@ export class Note extends Archetype {
                     effect.clips.good.play(0.02)
                     break
             }
+
+            const layout = Rect.one
+                .mul(2 * note.radius)
+                .scale(1, -1)
+                .translate(0, 1)
+
+            particle.effects.note.spawn(layout, 0.3, false)
 
             this.despawn = true
             return
