78b4468a4338b47e554513a331a80766ec4c1e51
Burrito <burritoyangster@gmail.com>
2024-12-05 01:28:13 +0800

    Initial commit

--- play/src/engine/playData/archetypes/Note.js
+++ play/src/engine/playData/archetypes/Note.js
@@ -2,6 +2,7 @@ import { EngineArchetypeDataName } from '@sonolus/core'
 
 import { note } from '../note.js'
 import { skin } from '../skin.js'
+import { windows } from '../windows.js'
 
 export class Note extends Archetype {
     import = this.defineImport({
@@ -13,6 +14,8 @@ export class Note extends Archetype {
     visualTime = this.entityMemory(Range)
 
     spawnTime = this.entityMemory(Number)
+
+    inputTime = this.entityMemory(Range)
 
     preprocess() {
         this.targetTime = bpmChanges.at(this.import.beat).time
@@ -30,7 +33,25 @@ export class Note extends Archetype {
         return time.now >= this.spawnTime
     }
 
+    initialize() {
+        this.inputTime.copyFrom(windows.good.add(this.targetTime).add(input.offset))
+    }
+
+    touch() {
+        if (time.now < this.inputTime.min) return
+
+        for (const touch of touches) {
+            if (!touch.started) continue
+
+            this.despawn = true
+            return
+        }
+    }
+
     updateParallel() {
+        if (time.now > this.inputTime.max) this.despawn = true
+        if (this.despawn) return
+
         const y = Math.unlerp(this.visualTime.min, this.visualTime.max, time.now)
 
         const layout = Rect.one.mul(note.radius).scale(1, -1).translate(0, y)
