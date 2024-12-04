c94418f183813fc256c6082b142c55df31486023
Burrito <burritoyangster@gmail.com>
2024-12-05 01:29:03 +0800

    Initial commit

--- watch/src/engine/watchData/effect.js
+++ watch/src/engine/watchData/effect.js
@@ -3,5 +3,7 @@ import { EffectClipName } from '@sonolus/core'
 export const effect = defineEffect({
     clips: {
         perfect: EffectClipName.Perfect,
+        great: EffectClipName.Great,
+        good: EffectClipName.Good,
     },
 })
