05f4b305d7f303eee3cb6a67cbf02f5296a91427
Burrito <burritoyangster@gmail.com>
2024-12-05 01:14:38 +0800

    Initial commit

--- preview/src/engine/previewData/skin.ts
+++ preview/src/engine/previewData/skin.ts
@@ -1,10 +1,20 @@
 import { SkinSpriteName } from '@sonolus/core'
 
+import { panel } from './panel.js'
+
 export const skin = defineSkin({
     sprites: {
         stageMiddle: SkinSpriteName.StageMiddle,
         stageLeftBorder: SkinSpriteName.StageLeftBorder,
         stageRightBorder: SkinSpriteName.StageRightBorder,
         note: SkinSpriteName.NoteHeadCyan,
+
+        beatLine: SkinSpriteName.GridNeutral,
     },
 })
+
+export const line = (sprite: SkinSprite, beat: number, a: number) => {
+    const pos = panel.getPos(bpmChanges.at(beat).time)
+
+    sprite.draw(Rect.one.scale(1.5, 0.005).add(pos), [0], a)
+}
