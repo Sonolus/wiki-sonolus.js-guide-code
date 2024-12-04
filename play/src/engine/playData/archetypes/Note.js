1a805172cd142657d9f005597b8a989b79bba1cf
Burrito <burritoyangster@gmail.com>
2024-12-05 01:28:07 +0800

    Initial commit

--- play/src/engine/playData/archetypes/Note.js
+++ play/src/engine/playData/archetypes/Note.js
@@ -5,6 +5,24 @@ export class Note extends Archetype {
         beat: { name: EngineArchetypeDataName.Beat, type: Number },
     })
 
+    targetTime = this.entityMemory(Number)
+
+    spawnTime = this.entityMemory(Number)
+
+    preprocess() {
+        this.targetTime = bpmChanges.at(this.import.beat).time
+
+        this.spawnTime = this.targetTime - 1
+    }
+
+    spawnOrder() {
+        return 1000 + this.spawnTime
+    }
+
+    shouldSpawn() {
+        return time.now >= this.spawnTime
+    }
+
     updateParallel() {
         debug.log(this.import.beat)
     }
