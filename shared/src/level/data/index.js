ae0ad7bdf884afcb48d82f6fd35154dfbb01a868
Burrito <burritoyangster@gmail.com>
2024-12-05 01:28:37 +0800

    Initial commit

--- shared/src/level/data/index.js
+++ shared/src/level/data/index.js
@@ -29,6 +29,20 @@ export const data = {
             ],
         })),
 
+        ...chart.timeScales.map(({ beat, timeScale }) => ({
+            archetype: EngineArchetypeName.TimeScaleChange,
+            data: [
+                {
+                    name: EngineArchetypeDataName.Beat,
+                    value: beat,
+                },
+                {
+                    name: EngineArchetypeDataName.TimeScale,
+                    value: timeScale,
+                },
+            ],
+        })),
+
         ...chart.notes.map((beat) => ({
             archetype: 'Note',
             data: [
